116857971554af3c28a8331bc5106d58ec25bd24
nagma <bano.nagma.1@gmail.com>
2025-10-19 16:36:19 +0530

    optimizing state updates

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,22 +1,8 @@
 import { useState, useEffect } from "react";
+import ProgressBar from "./ProgressBar";
 
 const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-       console.log('INTERVAL');
-      setRemainingTime(prevTime => prevTime -10);
-  }, 10);
-
-    return () => {
-      clearInterval(interval);
-      console.log("Cleaning up interval");
-    }
-  }, []);
-  
-  
   useEffect(() => {
     console.log("TIMER SET");
     const timer = setTimeout(() => {
@@ -41,7 +27,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER}/>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
